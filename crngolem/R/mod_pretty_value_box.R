575f385add9f08b6ef5fa8c440dd9ad32ed9c337
Gabriel Burcea <gb.burcea@gmail.com>
2023-01-17 16:38:01 +0000

    added some changes to try to get the right font. Could not succed.

--- crngolem/R/mod_pretty_value_box.R
+++ crngolem/R/mod_pretty_value_box.R
@@ -8,7 +8,7 @@
 
 # Module constants --------------------------------------------------------
 
-F_PRETTY_VALUE_BOX_TEMPLATE <- "inst/app/www/pretty_value_box_index.html"
+F_PRETTY_VALUE_BOX_TEMPLATE <- "inst/app/www/pretty_val_box_index.html"
 
 
 # Module UI ---------------------------------------------------------------
